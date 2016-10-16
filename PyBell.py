c28c697aab27a28b8a5971f4865b9b2d3a2f6ef1
Juan Pena <juan.pena@india.com>
2016-10-16 19:45:49 -0400

    Update PyBell.py

--- PyBell.py
+++ PyBell.py
@@ -1,5 +1,5 @@
 from pushbullet import Pushbullet
-
+import time
 import datetime
 import RPi.GPIO as GPIO
 
@@ -7,6 +7,8 @@ GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(24,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(7,GPIO.OUT)
+
+count = 0.0
 
 apiKey = "o.giFKbcb02CDCluBRGLZ9q5VwSXC6fWGz"
 pb = Pushbullet(apiKey)
@@ -16,12 +18,17 @@ push = pb.push_note('System is live', "Working...")
 while True:
     input_state = GPIO.input(24)
     if input_state == True:
-        push = pb.push_note('Door bell at ', str(datetime.datetime.now())[:16])
+        if count < 1:
+            push = pb.push_note('Door bell at ', str(datetime.datetime.now())[:16])
+            count -= 0.3
+         elif count > 4:
+            count = 2
         GPIO.output(7,1)
-        time.sleep(0.2)
+        time.sleep(.1)
+        count += 1
     else:
-        print 'nothing'
         GPIO.output(7,0)
-        time.sleep(0.2)
+        time.sleep(.1)
 GPIO.cleanup()
+
 
