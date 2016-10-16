8fadbf9c708d81005b8258c73afe9d7bd777c80a
Juan Pena <juan.pena@india.com>
2016-10-16 18:47:17 -0400

    Update PyBell.py

--- PyBell.py
+++ PyBell.py
@@ -5,13 +5,13 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(24,GPIO.IN)
+GPIO.setup(24,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(7,GPIO.OUT)
 
 apiKey = "o.giFKbcb02CDCluBRGLZ9q5VwSXC6fWGz"
 pb = Pushbullet(apiKey)
 
-push = pb.push_note('System is live', "chod")
+push = pb.push_note('System is live', "Working...")
 
 while True:
     input_state = GPIO.input(24)
